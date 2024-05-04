15f79ebd9a7994a569322a0c549a1ed7c9a82417
Aventador-k <kennedykangera@gmail.com>
2024-05-04 12:03:08 +0300

    updated joses file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+@extends('layout.app')
+@section('title','Home')
+@section('content')
+<section class="nav">
+    <a href="#" class="logo"><u>NAC</u></a>
+    <i class="fa-solid fa-bars" id="menu"></i>
+    <nav class="navbar">
+      <a href="#">HOME</a>
+      <a href="#">ABOUT</a>
+      <a href="#">CONTACT</a>
+    </nav>
+  </section>
+  <section class="section">
+    <div class="color"></div>
+    <div class="head">
+      <div class="content">
+        <u><h1>NAC</h1></u>
+        <h2>National Assessment Consortings</h2>
+        <p>
+          The ability to bring you good quality material, for knowledge
+          practice and brain juice.
+        </p>
+      </div>
+      <button type="submit">LEARN MORE</button>
+    </div>
+  </section>
+
+  <section class="b">
+    <div class="container">
+      <div class="image">
+        <img src="/images/outdore.jpg" alt="" />
+      </div>
+      <div class="content">
+        <h2><u>ABOUT-US</u></h2>
+        <p>
+          Welcome to National Assessment Consortings, your trusted platform
+          for online exams and assessments. Whether you're a student preparing
+          for a crucial exam, an educator looking to streamline the assessment
+          process, or an organization seeking reliable testing solutions,
+          we've got you covered.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="c">
+    <div class="container">
+      <div class="content">
+        <h2><u>OUR-MISSION</u></h2>
+        <p>
+          At National Assessment Consortings, our mission is to empower
+          learners and educators by providing a user-friendly, secure, and
+          efficient platform for conducting exams and assessments. We strive
+          to enhance the learning experience and facilitate fair evaluation
+          through innovative technology and comprehensive tools.
+        </p>
+      </div>
+      <div class="image">
+        <img src="/images/mathbord.jpg" alt="" />
+      </div>
+    </div>
+  </section>
+
+  <section class="d">
+    <div class="container">
+      <div class="image">
+        <img src="/images/yellow-man.jpg" alt="" />
+      </div>
+      <div class="content">
+        <h2><u>WHAT-SETS-US-APART</u></h2>
+        <p>
+          Quality Assurance: We partner with reputable publishers and content
+          providers to ensure that all materials available on our platform are
+          accurate, up-to-date, and aligned with the latest exam standards and
+          requirements. <br />
+          Competitive Pricing: Enjoy competitive prices on all our products,
+          with regular discounts and promotions to help you stretch your
+          budget further without compromising on quality. <br />
+          Exceptional Customer Service: Our dedicated customer support team is
+          here to assist you with any questions, concerns, or technical issues
+          you may encounter. We're committed to providing prompt, friendly,
+          and personalized service to ensure your shopping experience is
+          smooth and hassle-free.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <script src="/js.js"></script>
+
+
+@endsection
+
