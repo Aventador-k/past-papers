cd320c9c9132b0f0ecb9b522d68401492902ce73
Aventador-k <107771285+Aventador-k@users.noreply.github.com>
2025-01-08 13:49:38 +0300

    Delete resources directory
    had not cloned , now cloned to a new repository

--- resources/views/components/nav-bar.blade.php
+++
@@ -1,9 +0,0 @@
-<header class="header">
-    <a href="/" class="logo"><u>NAC</u></a>
-    <i class="fa-solid fa-bars" id="menu"></i>
-    <nav class="navbar">
-      <a href="#">HOME</a>
-      <a href="#b">ABOUT</a>
-      <a href="#d">CONTACT</a>
-    </nav>
-</header>
--- resources/views/welcome.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>I want to change this</h1>
