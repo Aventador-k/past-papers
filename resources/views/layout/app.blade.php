7821474c5c7f106f2ce2190ba197cf182e5215fa
kabwoyke <edwardkabwoy@gmail.com>
2025-01-16 12:16:43 +0300

    restored resources folder

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./font-awesome/css/all.css" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @stack("styles")
+    <title>NAC System-@yield('title')</title>
+</head>
+<body>
+    <div>
+        @yield('content')
+    </div>
+
+</body>
+</html>
+
